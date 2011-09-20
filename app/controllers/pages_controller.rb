90b50263c52cd5e21fbdde278ae82c3cabf18bfb
masay0shi <masayoshi.cdm@gmail.com>
2011-09-20 07:48:44 -0700

    Created Heroku App and Pages Controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def policy
+  end
+
+  def copyright
+  end
+
+  def contact
+  end
+
+end
---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'policy'" do
+    it "should be successful" do
+      get 'policy'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'copyright'" do
+    it "should be successful" do
+      get 'copyright'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+  end
+
+end
