f064daac89926c6b24b65934155887de8f74b122
masay0shi <masayoshi.cdm@gmail.com>
2011-09-20 07:17:55 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuseumCreated::Application.config.secret_token = '24722efc515567cede8c461f003052cd2c692889663d2cc13abc31d97f3ac41d8c8c34d63d26d5b88360768ad205af9b9986ff8505a472f1433b5054f98ee021'
