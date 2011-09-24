dd229b20dd37da7152238ecec76a99ec8d94856b
masay0shi <masayoshi.cdm@gmail.com>
2011-09-24 16:29:09 -0700

    Implemented static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
 
+  # Return a title on a per-page bassis.
+  def title
+    base_title = "MusiumCreated.com"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
+  # Display flash message in twitter-bootstrap
   def display_flash(name,msg)
     case name.to_s
     when "notice" then
