075c9f2ec297c932316b6703901eca540fdb9e67
masayoshi <masayoshi.cdm@gmail.com>
2013-07-28 01:08:06 -0700

    To fix devise 2.0 bug 2

--- db/migrate/20110921153515_devise_create_users.rb
+++ db/migrate/20110921153515_devise_create_users.rb
@@ -1,15 +1,34 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
+      #t.database_authenticatable :null => false
+      ### Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
-      #t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+
+      #t.recoverable
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      #t.rememberable
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      #t.trackable
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
 
       # t.encryptable
-      t.confirmable
+      #t.confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
       # t.lockable :lock_strategy => :failed_attempts, :unlock_strategy => :both
       # t.token_authenticatable
 
