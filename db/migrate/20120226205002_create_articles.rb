260bb0be4a8b9996f8952d472d98218d547f3cf7
masayoshi <masayoshi.cdm@gmail.com>
2013-07-28 01:45:11 -0700

    Skip create articles table temporarily

--- db/migrate/20120226205002_create_articles.rb
+++ db/migrate/20120226205002_create_articles.rb
@@ -1,11 +1,11 @@
 class CreateArticles < ActiveRecord::Migration
   def change
-    create_table :articles do |t|
-      t.string :title
-      t.string :url
-      t.text :summary
+    #create_table :articles do |t|
+      #t.string :title
+      #t.string :url
+      #t.text :summary
 
-      t.timestamps
-    end
+      #t.timestamps
+    #end
   end
 end
